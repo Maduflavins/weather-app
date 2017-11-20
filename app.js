02247f9ac0883392be343575bf35183f1b0e5855
Flavins <maduabuchiokonkwo@gmail.com>
2017-11-20 17:50:55 +0100

    error handling

--- app.js
+++ app.js
@@ -20,8 +20,16 @@ request({
     url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) =>{
-    console.log(`Address: ${body.results[0].formated_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+    if(error){
+        console.log('Unable to connect to Google services');
+
+    }else if(body.status === 'ZERO_RESLUTS'){
+        console.log('Unable to find that address.');
+    }else if(body.status === 'OK'){
+        console.log(`Address: ${body.results[0].formated_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+    }
+    
 
 })
