60d623c806bd93a560e92513c7df6e9e6b7bb3b1
Flavins <maduabuchiokonkwo@gmail.com>
2017-11-22 01:56:09 +0100

    reconfiguring using axions

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
-
+const axios = require('axios');
 const argv = yargs
     .options({
         a:{
@@ -15,19 +13,30 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, result) =>{
-    if(errorMessage){
-        console.log(errorMessage);
-    }else{
-        console.log(results.address);
-        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults)=>{
-            if(errorMessage){
-                console.log(errorMessage);
-            }else{
-                console.log(`its currently ${weatherResults.temperature}. it feels like ${weatherResults.apparentTemperature}`);
-            }
-        });
-        
-    }
-})
 
+    var encodedAddress = encodeURIComponent(argv.address);
+    var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+
+    axios.get(geocodeUrl).then((response) =>{
+        if(response.data.status === 'ZERO_RESULTS'){
+            throw new Error('Unable to find that address')
+        }
+        var lat = response.data.results[0].geometry.location.lat;
+        var lng = response.data.results[0].geometry.location.lng;
+        var weatherUrl = `https:appi.forcast.io/forecast/9eff3c48a48ddd19a8f7ca997c797b22/${lat},${lng}`
+        console.log(response.data.results[0].formatted_address);
+        return axios.get(weatherUrl);
+    }).then((response) =>{
+        var temperature = response.data.currently.temperature;
+        var apparentTemperature = response.data.currently.apparentTemperature;
+        console.log(`its currently ${temperature}. it feels like ${apparentTemperature}`);
+
+    }).catch((e) =>{
+        if(e.code === 'ENOTFOUND'){
+            console.log('Unable to connect to API server');
+        }else{
+            console.log(e.message);
+        }
+        
+    })
